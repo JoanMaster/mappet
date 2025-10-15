1524fd511414dc0b2fe7768e87659a56b139a7b0
joan castro <joscastrob7@gmail.com>
2025-10-15 02:04:19 -0500

    se crea evidencia sobre modulos creados y probados

---
+++ Adso_crud/tecnopunto/productos/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path 
+from . import views # Importamos las vistas de la aplicación productos 
+ 
+
+app_name = 'productos' 
+urlpatterns = [ 
+    path('', views.lista_productos, name='lista'), 
+    path('nuevo/', views.create_product, name='nuevo'), 
+    path('editar/<int:pk>/', views.edit_product, name='editar'), 
+    path('eliminar/<int:pk>/', views.delete_product, name='eliminar'), 
+] 
---
+++ Adso_crud/tecnopunto/tecnopunto/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin 
+from django.urls import path,include 
+from productos.views import inicio 
+ 
+
+urlpatterns = [ 
+    path('admin/', admin.site.urls), 
+    path('', inicio, name='inicio'), #ruta para la página de inicio 
+    path('productos/', include('productos.urls')), # ruta para las vistas de productos 
+] 
